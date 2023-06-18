09113a16642f0724ffe58b0cf311acbd4b70a2cd
milad <miladsoltani.dev@gmail.com>
2023-06-18 18:21:13 +0200

    first working commit for ubuntu

--- toggle-vpn.sh
+++ toggle-vpn.sh
@@ -1,5 +1,4 @@
-!#/bin/bash
-
+bash -c '
 vpn_con="soltani@ssl-vpn.vz-nrw.de";
 
 if [[ -n $(nmcli con show $vpn_con | grep "VPN connected") ]]; 
@@ -10,3 +9,4 @@ else
     nmcli con up $vpn_con; 
     gsettings set org.gnome.system.proxy mode "manual"; 
 fi; 
+'
