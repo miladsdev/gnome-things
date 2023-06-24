bbb0506f7f55200e4c2418099449c6cf74998b1a
milad <milad.soltani@verbraucherzentrale.nrw>
2023-06-24 13:02:40 +0200

    updated for arch at work

--- toggle-theme.sh
+++ toggle-theme.sh
@@ -6,13 +6,13 @@ if [[ $theme =~ "prefer-dark" ]];
 then 
     gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3" && gsettings set org.gnome.desktop.interface color-scheme "default"
     sed -i "s/\"workbench\.colorTheme\": \"Adwaita Dark & default syntax highlighting\",/\"workbench.colorTheme\": \"Adwaita Light \& default syntax highlighting\",/" "$HOME/.config/Code/User/settings.json"
-    sed -i 's/<global_color_scheme name="Dark" \/>/<global_color_scheme name="Light" \/>/' /home/milad/.config/JetBrains/PhpStorm*/options/colors.scheme.xml
-    sed -i 's/<laf class-name="com.intellij.ide.ui.laf.IntelliJLaf" themeId="ExperimentalDark" \/>/<laf class-name="com.intellij.ide.ui.laf.IntelliJLaf" themeId="ExperimentalLight" \/>/' /home/milad/.config/JetBrains/PhpStorm*/options/laf.xml
+    #sed -i 's/<global_color_scheme name="Dark" \/>/<global_color_scheme name="Light" \/>/' /home/milad/.config/JetBrains/PhpStorm*/options/colors.scheme.xml
+    #sed -i 's/<laf class-name="com.intellij.ide.ui.laf.IntelliJLaf" themeId="ExperimentalDark" \/>/<laf class-name="com.intellij.ide.ui.laf.IntelliJLaf" themeId="ExperimentalLight" \/>/' /home/milad/.config/JetBrains/PhpStorm*/options/laf.xml
 
 else 
     gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark" && gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
     sed -i "s/\"workbench\.colorTheme\": \"Adwaita Light & default syntax highlighting\",/\"workbench.colorTheme\": \"Adwaita Dark \& default syntax highlighting\",/" "$HOME/.config/Code/User/settings.json"
-    sed -i 's/<global_color_scheme name="Light" \/>/<global_color_scheme name="Dark" \/>/' /home/milad/.config/JetBrains/PhpStorm*/options/colors.scheme.xml
-    sed -i 's/<laf class-name="com.intellij.ide.ui.laf.IntelliJLaf" themeId="ExperimentalLight" \/>/<laf class-name="com.intellij.ide.ui.laf.IntelliJLaf" themeId="ExperimentalDark" \/>/' /home/milad/.config/JetBrains/PhpStorm*/options/laf.xml
+    #sed -i 's/<global_color_scheme name="Light" \/>/<global_color_scheme name="Dark" \/>/' /home/milad/.config/JetBrains/PhpStorm*/options/colors.scheme.xml
+    #sed -i 's/<laf class-name="com.intellij.ide.ui.laf.IntelliJLaf" themeId="ExperimentalLight" \/>/<laf class-name="com.intellij.ide.ui.laf.IntelliJLaf" themeId="ExperimentalDark" \/>/' /home/milad/.config/JetBrains/PhpStorm*/options/laf.xml
 
 fi
